a63aeae843af3d833f41a24a5ea698807f6b6640
bllli <me@bllli.cn>
2018-04-01 22:33:29 +0800

    setting change to sqlite

--- REReverseCourse/settings.py
+++ REReverseCourse/settings.py
@@ -81,7 +81,14 @@ WSGI_APPLICATION = 'REReverseCourse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = user_config.DATABASES
+
+# DATABASES = user_config.DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
